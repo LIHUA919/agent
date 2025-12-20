eb99747af6b543e6e3ddf536f802509115227844
Lihua <1017343802@qq.com>
2025-12-20 21:18:35 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -41,13 +41,6 @@ class Student implements Person {
     }
 }
 
-// interface
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Student("Xiao Ming");
-        p.run();
-    }
-}
 
 
 
