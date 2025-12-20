9002f6108f37846b9482bfc9f5d8435698c0b88d
Lihua <1017343802@qq.com>
2025-12-20 21:05:57 +0800

    Remove constructor from Person class
    Removed constructor from Person class.

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -11,10 +11,7 @@ class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+  
     
   
 
