acc23811d416c9627afb5032ec33fc15099666fa
Lihua <1017343802@qq.com>
2025-12-20 21:14:22 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -22,3 +22,21 @@ interface Person {
     void run();
     String getName();
 }
+
+class Student implements Person {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.name + " run");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
