59f733773f06b4acfebf488f6d6cc43e388cf255
Lihua <1017343802@qq.com>
2025-12-20 20:49:25 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -36,4 +36,28 @@ class Student extends Person {
     }
 }
 
+// super
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student("Xiao Ming", 12, 89);
+    }
+}
+
+class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    protected int score;
+
+    public Student(String name, int age, int score) {
+        this.score = score;
+    }
+}
 
