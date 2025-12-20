4c7ea0c457ae8d7af42a5faace942c5274af18df
Lihua <1017343802@qq.com>
2025-12-20 18:44:38 +0800

    Add main method to print array length

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -8,5 +8,16 @@ public class Main {
         };
         System.out.println(ns.length); // 3
     }
+
+    public static void main(String[] args) {
+        int[][] ns = {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 10, 11, 12 }
+        };
+        int[] arr0 = ns[0];
+        System.out.println(arr0.length); // 4
+    }
 }
+
 
