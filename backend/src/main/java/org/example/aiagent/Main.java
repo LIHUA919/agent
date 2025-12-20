d605d5f3006215f50671d7e37944091e16eec0f6
Lihua <1017343802@qq.com>
2025-12-20 21:06:22 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,11 +1,4 @@
-// 构造方法
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Person("Xiao Ming", 15);
-        System.out.println(p.getName());
-        System.out.println(p.getAge());
-    }
-}
+
 
   
     
