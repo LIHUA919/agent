0f5419e06506a61e8e0f3ec6b52ca3bb611c1fa3
Lihua <1017343802@qq.com>
2025-12-28 18:21:22 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,4 +1,18 @@
+// try...catch
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        byte[] bs = toGBK("中文");
+        System.out.println(Arrays.toString(bs));
+    }
+
+    static byte[] toGBK(String s) throws UnsupportedEncodingException {
+        // 用指定编码转换String为byte[]:
+        return s.getBytes("GBK");
+    }
+}
 
 
  
