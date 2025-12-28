f00955496582ec6a4ea67b460832791fcd19fede
Lihua <1017343802@qq.com>
2025-12-28 18:14:35 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,1 +1,15 @@
 
+// try...catch
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        byte[] bs = toGBK("中文");
+        System.out.println(Arrays.toString(bs));
+    }
+
+    static byte[] toGBK(String s) {
+        return s.getBytes("GBK");
+    }
+}
