03b391024556f59232a404dcefe8671d4bffd0e5
Lihua <1017343802@qq.com>
2025-12-28 18:33:24 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -5,3 +5,17 @@ void process(String file) throws IOException {
         System.out.println("END");
     }
 }
+
+public static void main(String[] args) {
+    try {
+        process1();
+        process2();
+        process3();
+    } catch (IOException e) {
+        System.out.println("Bad input");
+    } catch (NumberFormatException e) {
+        System.out.println("Bad input");
+    } catch (Exception e) {
+        System.out.println("Unknown error");
+    }
+}
