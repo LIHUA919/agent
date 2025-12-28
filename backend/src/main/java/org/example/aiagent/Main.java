346e3857ac3eb6617041280af1e36157e2c84393
Lihua <1017343802@qq.com>
2025-12-28 18:32:11 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,10 +1,7 @@
 public static void main(String[] args) {
     try {
         process1();
-        process2();
-        process3();
-    } catch (IOException e) {
-        System.out.println(e);
+ 
 
 
 
