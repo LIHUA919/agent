0c93387dd26a20a21a7cf5152593fe02baf53c86
Lihua <1017343802@qq.com>
2025-12-28 18:19:09 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -15,8 +15,6 @@ public class Main {
     static byte[] toGBK(String s) throws UnsupportedEncodingException {
         // 用指定编码转换String为byte[]:
         return s.getBytes("GBK");
-    }
-}
 
 
        
