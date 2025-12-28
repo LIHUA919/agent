29026238f864a95370b1ef3419eda5125fad8e23
Lihua <1017343802@qq.com>
2025-12-28 18:04:48 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,1 +1,14 @@
-
+int code = processFile("C:\\test.txt");
+if (code == 0) {
+    // ok:
+} else {
+    // error:
+    switch (code) {
+    case 1:
+        // file not found:
+    case 2:
+        // no read permission:
+    default:
+        // unknown error:
+    }
+}
