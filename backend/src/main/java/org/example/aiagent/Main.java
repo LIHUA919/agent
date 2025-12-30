d0e56a2dab79218e91915ec1f5d2a30112e9d96e
Lihua <1017343802@qq.com>
2025-12-30 08:29:30 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -23,3 +23,21 @@ public String[] readLinesFromFile(String file) {
     }
     ...
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println(p.address.city.toLowerCase());
+    }
+}
+
+class Person {
+    String[] name = new String[2];
+    Address address = new Address();
+}
+
+class Address {
+    String city;
+    String street;
+    String zipcode;
+}
