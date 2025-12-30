7bd6278694c84461426492b73bc47d5c7f94ffb3
Lihua <1017343802@qq.com>
2025-12-30 08:02:18 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             process1();
-        } catch (Exception e) {
-            e.printStackTrace();
+  
      
   
