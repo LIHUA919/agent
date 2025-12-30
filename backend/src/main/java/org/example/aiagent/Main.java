0d9a045eec46f2449ad6e357f042efcd5a1cf5d7
Lihua <1017343802@qq.com>
2025-12-30 08:05:55 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -25,6 +25,30 @@ void process2(String s) {
     }
 }
 
+// exception
+public class Main {
+    public static void main(String[] args) {
+        try {
+            process1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void process1() {
+        try {
+            process2();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    static void process2() {
+        throw new NullPointerException();
+    }
+}
+
+
 
 
      
