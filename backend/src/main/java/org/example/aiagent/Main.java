29bf6a468de511d7eef6964dd68304aee6de7e7a
Lihua <1017343802@qq.com>
2025-12-31 08:25:01 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,1 +1,9 @@
 
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File f = new File("C:\\Windows\\notepad.exe");
+        System.out.println(f);
+    }
+}
