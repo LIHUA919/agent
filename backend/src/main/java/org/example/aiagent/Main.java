b8dbf63b225e9bf5a7528405f54ebc2a4aa62609
Lihua <1017343802@qq.com>
2025-12-31 08:32:28 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File f = new File("C:\\Windows");
         File[] fs1 = f.listFiles(); // 列出所有文件和子目录
-        printFiles(fs1);
-        File[] fs2 = f.listFiles(new FilenameFilter() { // 仅列出.exe文件
+ 
 
 
 
