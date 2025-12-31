98f2d808a37ae05698c36fb0a1006545c52549a3
Lihua <1017343802@qq.com>
2025-12-31 08:25:52 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -21,3 +21,20 @@ public class Main {
     }
 }
 
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File f1 = new File("C:\\Windows");
+        File f2 = new File("C:\\Windows\\notepad.exe");
+        File f3 = new File("C:\\Windows\\nothing");
+        System.out.println(f1.isFile());
+        System.out.println(f1.isDirectory());
+        System.out.println(f2.isFile());
+        System.out.println(f2.isDirectory());
+        System.out.println(f3.isFile());
+        System.out.println(f3.isDirectory());
+    }
+}
+
