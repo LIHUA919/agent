b5eb2f7461ebc0cbfccd84ff5d4d0f8b4994b554
Lihua <1017343802@qq.com>
2025-12-31 08:41:41 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,9 +1,4 @@
 
-
-
-        int n;
-        StringBuilder sb = new StringBuilder();
-        while ((n = input.read()) != -1) {
             sb.append((char) n);
         }
         return sb.toString();
