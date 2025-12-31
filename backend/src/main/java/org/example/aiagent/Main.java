76dad37483bd96499bf0567f132e858fd628645d
Lihua <1017343802@qq.com>
2025-12-31 08:32:34 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -2,8 +2,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File f = new File("C:\\Windows");
-        File[] fs1 = f.listFiles(); // 列出所有文件和子目录
+        F
  
 
 
