a65b2230850e57df58b72bbe06fe93df311d511f
Lihua <1017343802@qq.com>
2025-12-31 20:20:32 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -2,6 +2,5 @@ public class BookService {
     private HikariConfig config = new HikariConfig();
     private DataSource dataSource = new HikariDataSource(config);
 
-    public Book getBook(long bookId) {
         
        
