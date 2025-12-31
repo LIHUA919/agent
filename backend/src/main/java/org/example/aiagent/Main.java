d2df471c3faa3f4b74c9304b279f48230ae400cc
Lihua <1017343802@qq.com>
2025-12-31 20:18:06 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -26,7 +26,4 @@ public class CartServlet extends HttpServlet {
     private BookService bookService = new BookService();
     private UserService userService = new UserService();
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        long currentUserId 
-        
 
