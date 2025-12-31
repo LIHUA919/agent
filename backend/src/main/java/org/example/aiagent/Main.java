29ddfad48edaf5b954881f120d5c4aa729237251
Lihua <1017343802@qq.com>
2025-12-31 08:36:30 +0800

    Implement file reading in Main class
    Added main method to read and print content from README.txt

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -44,3 +44,23 @@ public class Main {
         return sb.toString();
     }
 }
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String s;
+        try (InputStream input = new FileInputStream("C:\\test\\README.txt")) {
+            s = readAsString(input);
+        }
+        System.out.println(s);
+    }
+
+    public static String readAsString(InputStream input) throws IOException {
+        int n;
+        StringBuilder sb = new StringBuilder();
+        while ((n = input.read()) != -1) {
+            sb.append((char) n);
+        }
+        return sb.toString();
+    }
+}
+
