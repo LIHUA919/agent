c5e282d040a0a187ba20961ccf0ea6f939b7c551
Lihua <1017343802@qq.com>
2025-12-31 08:25:35 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -9,3 +9,15 @@ public class Main {
 }
 
 File f = new File("C:\\Windows\\notepad.exe");
+
+ import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File f = new File("..");
+        System.out.println(f.getPath());
+        System.out.println(f.getAbsolutePath());
+        System.out.println(f.getCanonicalPath());
+    }
+}
+
