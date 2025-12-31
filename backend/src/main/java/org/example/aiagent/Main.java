e9960f3445ac09a70e9e1fd95681008887aa0600
Lihua <1017343802@qq.com>
2025-12-31 08:40:11 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -1,7 +1,5 @@
 
 
-    public static String readAsString(InputStream input) throws IOException {
-        int n;
         StringBuilder sb = new StringBuilder();
         while ((n = input.read()) != -1) {
             sb.append((char) n);
