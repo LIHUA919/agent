876708a4d3752c2f0813b0e003720590270afbba
Lihua <1017343802@qq.com>
2025-12-31 20:23:46 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -2,6 +2,4 @@ public class CartServlet extends HttpServlet {
     private BookService bookService = new BookService();
     private UserService userService = new UserService();
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        long currentUserId = getFromCookie(req);
 
