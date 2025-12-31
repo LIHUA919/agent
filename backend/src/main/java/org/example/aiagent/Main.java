1ef4fecfb63bae076aa1b24427dd8fbeedc567d3
Lihua <1017343802@qq.com>
2025-12-31 08:26:57 +0800

    Update Main.java

--- backend/src/main/java/org/example/aiagent/Main.java
+++ backend/src/main/java/org/example/aiagent/Main.java
@@ -4,9 +4,6 @@
  
  import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        File f = new File("..");
         System.out.println(f.getPath());
         System.out.println(f.getAbsolutePath());
         System.out.println(f.getCanonicalPath());
